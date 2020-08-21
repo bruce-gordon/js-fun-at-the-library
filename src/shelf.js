e950b801fde75b520c09cc835275aea843d00160
Bruce Gordon <brc.gordon@gmail.com>
2020-08-20 21:37:12 -0600

    Add code for shelfBook

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,16 @@
+function shelfBook(book, shelfArray) {
+  if (shelfArray.length < 3) {
+  shelfArray.unshift(book);
+  }
+}
 
+function unshelfBook() {
+
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
   // listTitles: listTitles,
   // searchShelf: searchShelf
 };
