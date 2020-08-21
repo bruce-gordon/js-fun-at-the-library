8e3163e1531dd91d27722dce0eb3a426e31d6a10
Bruce Gordon <brc.gordon@gmail.com>
2020-08-21 17:54:23 -0600

    Add functions for createLibrary and addBook

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,31 @@
+function createLibrary(libraryName) {
+  return {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+      }
+  }
+}
 
+function addBook(library, book) {
+  if (book.genre === 'fantasy') {
+    library.shelves.fantasy.push(book);
+  } else if (book.genre === 'nonFiction') {
+    library.shelves.nonFiction.push(book);
+  } else if (book.genre === 'fiction') {
+    library.shelves.fiction.push(book);
+  }
+}
+
+function checkoutBook(library, bookTitle) {
+  library.shelves.fiction.shift();
+  return 'You have now checked out ' + bookTitle + ' from the ' + library.name;
+}
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+  createLibrary: createLibrary,
+  addBook: addBook,
+  checkoutBook: checkoutBook
 };
