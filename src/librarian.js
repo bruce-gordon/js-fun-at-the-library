46cc05a269e27ecbf917bfaf38ef3e061c882cc3
Bruce Gordon <brc.gordon@gmail.com>
2020-08-22 15:07:03 -0600

    Add greetPatron and findBook behaviors to Librarian class

--- src/librarian.js
+++ src/librarian.js
@@ -2,10 +2,25 @@ var checkoutBook = require('../src/library.js').checkoutBook;
 var searchShelf = require('../src/shelf.js').searchShelf;
 
 class Librarian {
-  constructor() {
-
+  constructor(name, library) {
+    this.name = name;
+    this.library = library
   }
-  
+  greetPatron(patronName, isMorning) {
+    if (isMorning === true) {
+    return "Good morning, " + patronName + "!";
+    } else {
+    return "Hello, " + patronName + "!"
+    }
+  }
+  findBook(title) {
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++)
+    {
+    if (this.library.shelves.fantasy[i].title === title) {
+      return "Yes, we have " + title
+      }
+    } return "Sorry, we do not have " + title
+  }
 }
 
 module.exports = Librarian;
